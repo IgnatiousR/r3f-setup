76cc2db2f256f1ed236c4e4e12912af135a85de2
Ignatious <ignatiousr80@gmail.com>
2026-02-04 16:21:33 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-
+import * as THREE from "three";
 function App() {
+  // console.log(THREE.MathUtils.degToRad(125));
   return (
     <Canvas
       camera={{
         position: [3, 3, 3],
       }}
     >
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <OrbitControls />
+      <mesh scale-z={4} rotation-y={125 * (Math.PI / 180)}>
+        <boxGeometry args={[1, 1, 1]} />
+        {/* <meshBasicMaterial color={0x00ff00} /> */}
+        <meshStandardMaterial color="green" />
+        {/* <meshNormalMaterial /> */}
       </mesh>
+
+      <mesh position-x={4} position-z={-1} scale-z={4} rotation-y={THREE.MathUtils.degToRad(125)}>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" side={THREE.DoubleSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={0.5} />
     </Canvas>
   );
 }
