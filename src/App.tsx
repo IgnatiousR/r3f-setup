3cb22efc9b923ed732d8d43356a1e4dda18f1acf
Ignatious <ignatiousr80@gmail.com>
2026-02-05 18:20:32 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,56 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stats, useHelper } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useRef } from "react";
 import * as THREE from "three";
-function App() {
-  // console.log(THREE.MathUtils.degToRad(125));
-  return (
-    <Canvas
-      camera={{
-        position: [3, 3, 3],
-      }}
-    >
-      <OrbitControls />
-      <mesh scale-z={4} rotation-y={125 * (Math.PI / 180)}>
-        <boxGeometry args={[1, 1, 1]} />
-        {/* <meshBasicMaterial color={0x00ff00} /> */}
-        <meshStandardMaterial color="green" />
-        {/* <meshNormalMaterial /> */}
-      </mesh>
+import { Leva, LevaPanel, useControls } from "leva";
 
-      <mesh position-x={4} position-z={-1} scale-z={4} rotation-y={THREE.MathUtils.degToRad(125)}>
+const Box = () => {
+  const ref = useRef();
+  useHelper(ref, THREE.BoxHelper, "red");
+
+  const { position } = useControls({
+    position: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+  });
+
+  return (
+    <mesh ref={ref} position={[position.x, position.y, position.z]}>
+      <boxGeometry />
+      <meshStandardMaterial color={"white"} transparent opacity={0} />
+    </mesh>
+  );
+};
+function App() {
+  return (
+    <>
+      {/* <Leva hidden /> */}
+      <Stats />
+      <Canvas
+        camera={{
+          position: [3, 3, 3],
+        }}
+      >
+        <OrbitControls />
+        {/* <mesh scale-z={4} rotation-y={125 * (Math.PI / 180)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={0x00ff00} />
+        <meshStandardMaterial color="green" />
+        <meshNormalMaterial />
+      </mesh> */}
+
+        {/* <mesh position-x={4} position-z={-1} scale-z={4} rotation-y={THREE.MathUtils.degToRad(125)}>
         <boxGeometry />
         <meshStandardMaterial color="hotpink" side={THREE.DoubleSide} />
-      </mesh>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0, 3]} intensity={1} />
-      <directionalLight position={[0, 3, 3]} intensity={0.5} />
-    </Canvas>
+      </mesh> */}
+        <Box />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 0, 3]} intensity={1} />
+        <directionalLight position={[0, 3, 3]} intensity={0.5} />
+      </Canvas>
+    </>
   );
 }
 
